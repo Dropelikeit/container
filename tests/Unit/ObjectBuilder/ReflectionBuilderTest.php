7c38c5637dc6010f6eaa1ffb97399257fcd2e8c4
Marcel Strahl <info@marcel-strahl.de>
2024-01-12 19:49:03 +0100

    Add GitHub Actions CI workflow and additional PHP classes
    This commit introduces a new file for CI workflows integrating GitHub Actions for testing different PHP versions. It also adds new ObjectBuilder and FileLoader classes and their corresponding unit tests. Furthermore, the configuration for PHPUnit, php-cs-fixer, and Infection has been added. '.gitignore' file has also been created to exclude certain files and directories from versioning.

---
+++ tests/Unit/ObjectBuilder/ReflectionBuilderTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MarcelStrahl\Tests\Unit\ObjectBuilder;
+
+use LogicException;
+use MarcelStrahl\Container\Exception\ObjectBuilder\CanNotCreateClassWithNoneClassDependencies;
+use MarcelStrahl\Container\ObjectBuilder\ObjectBuilder;
+use MarcelStrahl\Container\ObjectBuilder\ReflectionBuilder;
+use MarcelStrahl\Tests\Unit\ObjectBuilder\_data\SimpleTestServiceWithConstructorAndNonClassDependency;
+use MarcelStrahl\Tests\Unit\ObjectBuilder\_data\SimpleTestServiceWithConstructorAndOneDependency;
+use MarcelStrahl\Tests\Unit\ObjectBuilder\_data\SimpleTestServiceWithConstructorButWithoutDependencies;
+use MarcelStrahl\Tests\Unit\ObjectBuilder\_data\SimpleTestServiceWithoutConstructor;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ *
+ */
+final class ReflectionBuilderTest extends TestCase
+{
+    public function testCanInitialize(): void
+    {
+        $builder = new ReflectionBuilder();
+
+        static::assertInstanceOf(ObjectBuilder::class, $builder);
+    }
+
+    public function testCanInitializeSimpleClassWithoutConstructor(): void
+    {
+        $builder = new ReflectionBuilder();
+
+        $object = $builder->initialize(SimpleTestServiceWithoutConstructor::class);
+
+        static::assertInstanceOf(SimpleTestServiceWithoutConstructor::class, $object);
+    }
+
+    public function testCanInitializeSimpleClassWithConstructorButWithoutDependencies(): void
+    {
+        $builder = new ReflectionBuilder();
+
+        $object = $builder->initialize(SimpleTestServiceWithConstructorButWithoutDependencies::class);
+
+        static::assertInstanceOf(SimpleTestServiceWithConstructorButWithoutDependencies::class, $object);
+    }
+
+    public function testCanInitializeSimpleClassWithConstructorAndOneDependency(): void
+    {
+        $builder = new ReflectionBuilder();
+
+        $object = $builder->initialize(SimpleTestServiceWithConstructorAndOneDependency::class);
+
+        static::assertInstanceOf(SimpleTestServiceWithConstructorAndOneDependency::class, $object);
+    }
+
+    public function testCanNotInitializeNonExistingClass(): void
+    {
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('Cannot find your class, try `composer dumpautoload` command.');
+
+        $builder = new ReflectionBuilder();
+
+        $builder->initialize('X\Dummy\Namespace\Class');
+    }
+
+    public function testCanNotInitializeNonClassDependency(): void
+    {
+        $this->expectException(CanNotCreateClassWithNoneClassDependencies::class);
+        $this->expectExceptionMessage('Currently, no object can be created with non-class dependencies, given type "int".');
+
+        $builder = new ReflectionBuilder();
+
+        $builder->initialize(SimpleTestServiceWithConstructorAndNonClassDependency::class);
+    }
+
+    /**
+     * @test
+     */
+    public function throwLogicExceptionWhenReflectionBuilderIsPassedCallables(): void
+    {
+        $this->expectException(LogicException::class);
+        $this->expectExceptionMessage('callables are not yet supported.');
+
+        $closure = static function (): string {
+            return 'dummy';
+        };
+
+        $builder = new ReflectionBuilder();
+
+        $builder->initialize($closure);
+    }
+}
---
+++ tests/Unit/ObjectContainerTest.php
@@ -0,0 +1,157 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MarcelStrahl\Tests\Unit;
+
+use MarcelStrahl\Container\Delegator\DelegateInterface;
+use MarcelStrahl\Container\Dto\ObjectStoreInterface;
+use MarcelStrahl\Container\Exception\NotFoundInContainerException;
+use MarcelStrahl\Container\ObjectContainer;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+
+/**
+ * @internal
+ *
+ */
+final class ObjectContainerTest extends TestCase
+{
+    /**
+     * @psalm-var MockObject&ObjectStoreInterface
+     */
+    private MockObject/* &ObjectStoreInterface */ $store;
+
+    /**
+     * @psalm-var MockObject&DelegateInterface
+     */
+    private MockObject/* &ObjectBuilder */ $delegator;
+
+    protected function setUp(): void
+    {
+        $this->store = $this->createMock(ObjectStoreInterface::class);
+        $this->delegator = $this->createMock(DelegateInterface::class);
+    }
+
+    public function testCanInitialize(): void
+    {
+        $container = new ObjectContainer($this->store, $this->delegator);
+
+        $this->assertInstanceOf(ContainerInterface::class, $container);
+    }
+
+    public function testCanGetObjectFromContainer(): void
+    {
+        $dummy = new class() {};
+
+        $this->store
+            ->expects(static::once())
+            ->method('searchById')
+            ->with($dummy::class)
+            ->willReturn($dummy)
+        ;
+
+        $container = new ObjectContainer($this->store, $this->delegator);
+
+        $object = $container->get($dummy::class);
+
+        static::assertInstanceOf($dummy::class, $object);
+    }
+
+    public function testCanNotGetContainerWhenClassNotExist(): void
+    {
+        $dummy = new class() {};
+
+        $this->expectException(NotFoundInContainerException::class);
+        $this->expectExceptionMessage(sprintf(
+            'Can not found a entry in container with given id "%s"',
+            $dummy::class,
+        ));
+
+        $exception = new \LogicException();
+
+        $this->delegator
+            ->expects(self::once())
+            ->method('delegate')
+            ->with($dummy::class)
+            ->willThrowException($exception)
+        ;
+
+        $this->store
+            ->expects(self::once())
+            ->method('searchById')
+            ->with($dummy::class)
+            ->willReturn(null)
+        ;
+
+        $container = new ObjectContainer($this->store, $this->delegator);
+
+        $object = $container->get($dummy::class);
+
+        $this->assertInstanceOf($dummy::class, $object);
+    }
+
+    public function testInitializeObjectWhenClassNotExistInObjectContainer(): void
+    {
+        $dummy = new class() {};
+
+        $this->delegator
+            ->expects(static::once())
+            ->method('delegate')
+            ->with($dummy::class)
+            ->willReturn($dummy)
+        ;
+
+        $this->store
+            ->expects(static::once())
+            ->method('searchById')
+            ->with($dummy::class)
+            ->willReturn(null)
+        ;
+
+        $this->store
+            ->expects(static::once())
+            ->method('append')
+            ->with($dummy::class, $dummy)
+        ;
+
+        $container = new ObjectContainer($this->store, $this->delegator);
+
+        $container->get($dummy::class);
+
+        static::assertInstanceOf($dummy::class, $dummy);
+    }
+
+    public function testCheckContainerHasObject(): void
+    {
+        $dummy = new class() {};
+
+        $this->store
+            ->expects(static::once())
+            ->method('searchById')
+            ->with($dummy::class)
+            ->willReturn($dummy)
+        ;
+
+        $container = new ObjectContainer($this->store, $this->delegator);
+
+        static::assertTrue($container->has($dummy::class));
+    }
+
+    public function testCheckContainerHasNotObject(): void
+    {
+        $dummy = new class() {};
+
+        $this->store
+            ->expects(static::once())
+            ->method('searchById')
+            ->with($dummy::class)
+            ->willReturn(null)
+        ;
+
+        $container = new ObjectContainer($this->store, $this->delegator);
+
+        static::assertFalse($container->has($dummy::class));
+    }
+}
